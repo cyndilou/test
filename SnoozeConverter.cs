10672d33f906a49ec1fd65646749ff698d6bb8a9
Cyndi A <cdanschutz@gmail.com>
2012-02-25 13:16:10 -0600

    change from red to yellow

---
+++ SnoozeConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace GoogleCalendarReminder.Converters
+{
+    public class SnoozeConverter : IValueConverter
+    {
+        public static readonly int CustomSnoozeValue = -100;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null) return String.Empty;
+
+            var snooze = 0;
+            try
+            {
+                snooze = (int)value;
+            }
+            catch (InvalidCastException)
+            {
+                return String.Empty;
+            }
+
+            if (snooze == CustomSnoozeValue)
+            {
+                return "<Custom>";
+            }
+
+            if (snooze < 0)
+            {
+                return (-1 * snooze) + " minutes before start";
+            }
+
+            return snooze + " minutes";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
